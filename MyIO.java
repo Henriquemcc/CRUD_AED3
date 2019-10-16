2bd52386fd2737efc0c2ffc6ac214371cfb0ffae
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2019-10-16 09:18:16 -0300

    Inserindo comentarios na classe MyIO

--- MyIO.java
+++ MyIO.java
@@ -1,3 +1,4 @@
+package packageone;
 import java.io.*;
 import java.nio.charset.*;
 import java.util.InputMismatchException;
@@ -9,12 +10,19 @@ class MyIO
    private static BufferedReader in = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.ISO_8859_1));
    private static String charset = "ISO-8859-1";
 
+   /**
+    * Funcao do metodo: Este metodo serve para altrar o charset da classe MyIO
+    * @param charset_ Novo charset.
+    */
    public static void setCharset(String charset_)
    {
       charset = charset_;
       in = new BufferedReader(new InputStreamReader(System.in, Charset.forName(charset)));
-   }
+   }//fim do metodo setCharset
 
+   /**
+    * Funcao do metodo: Este metodo serve para imprimir um texto na tela do usuario.
+    */
    public static void print()
    {
       try
@@ -30,8 +38,12 @@ class MyIO
       {
          e.printStackTrace();
       }
-   }
+   }//fim do metodo print
 
+   /**
+    * Funcao do metodo: Este metodo serve para imprimir um numero na tela do usuario.
+    * @param x Numero inteiro que sera impresso.
+    */
    public static void print(int x)
    {
       try
@@ -47,8 +59,12 @@ class MyIO
       {
          e.printStackTrace();
       }
-   }
+   }//fim do metodo print
 
+   /**
+    * Funcao do metodo: Este metodo serve para imprimir um numero real na tela do usuario.
+    * @param x Numero real que sera impresso na tela do usuario.
+   */
    public static void print(double x)
    {
       try
@@ -65,8 +81,12 @@ class MyIO
          e.printStackTrace();
       }
 
-   }
+   }//fim do metodo print
 
+   /**
+    * Funcao do metodo: Este metodo serve para imprimir uma String na tela do usuario.
+    * @param x String que sera impressa na tela do usuario.
+    */
    public static void print(String x)
    {
       try
@@ -82,8 +102,12 @@ class MyIO
       {
          e.printStackTrace();
       }
-   }
+   }//fim do metodo print
 
+   /**
+    * Funcao do metodo: Este metodo serve para imprimir um valor booleano na tela do usuario.
+    * @param x Valor booleano que sera impresso na tela do usuario.
+    */
    public static void print(boolean x)
    {
       try
@@ -99,8 +123,12 @@ class MyIO
       {
          e.printStackTrace();
       }
-   }
+   }//fim do metodo print
 
+   /**
+    * Funcao do metodo: Este metodo serve para imprimir um caractere na tela do usuario.
+    * @param x Caractere que sera impresso na tela do usuario.
+    */
    public static void print(char x)
    {
       try 
@@ -116,8 +144,11 @@ class MyIO
       {
          e.printStackTrace();
       }
-   }
+   }//fim do metodo print
 
+   /**
+    * Funcao do metodo: Este metodo serve para imprimir uma quebra de linha na tela do usuario.
+    */
    public static void println()
    {
       try
@@ -133,8 +164,12 @@ class MyIO
       {
          e.printStackTrace();
       }
-   }
+   }//fim do metodo println
 
+   /**
+    * Funcao do metodo: Este metodo serve para imprimir um numero inteiro mais uma quebra de linha na tela do usuario.
+    * @param x Numero inteiro que sera impresso na tela do usuario.
+    */
    public static void println(int x)
    {
       try
@@ -150,8 +185,12 @@ class MyIO
       {
          e.printStackTrace();
       }
-   }
+   }//fim do metodo println
 
+   /**
+    * Funcao do metodo: Este metodo serve para imprimir um numero inteiro (do tipo byte) mais uma quebra de linha na tela do usuario.
+    * @param x Numero inteiro (de tipo byte) que sera impresso na tela do usuario.
+    */
    public static void println(byte x)
    {
       try
@@ -167,8 +206,12 @@ class MyIO
       {
          e.printStackTrace();
       }
-   }
+   }//fim do metodo println
 
+   /**
+    * Funcao do metodo: Este metodo serve para imprimir um numero inteiro (do tipo short) mais uma quebra de linha na tela do usuario.
+    * @param x Numero inteiro (de tipo short) que sera impresso na tela do usuario.
+    */
    public static void println(short x)
    {
       try
@@ -184,8 +227,12 @@ class MyIO
       {
          e.printStackTrace();
       }
-   }
+   }//fim do metodo println
 
+   /**
+    * Funcao do metodo: Este metodo serve para imprimir um numero real (de tipo double) na tela do usuario mais uma quebra de linha na tela do usuario.
+    * @param x Numero real (de tipo double) que sera impresso na tela do usuario.
+    */
    public static void println(double x)
    {
       try 
@@ -201,8 +248,12 @@ class MyIO
       {
          e.printStackTrace();
       }
-   }
+   }//fim do metodo println
 
+   /**
+    * Funcao do metodo: Este metodo serve para imprimir uma String mais uma quebra de linha na tela do usuario.
+    * @param x String que sera impressa na tela do usuario.
+    */
    public static void println(String x)
    {
       try 
@@ -218,8 +269,12 @@ class MyIO
       {
          e.printStackTrace();
       }
-   }
+   }//fim do metodo println
 
+   /**
+    * Funcao do metodo: Este metodo serve para imprimir um valor booleano mais uma quebra de linha na tela do usuario.
+    * @param x Valor booleano que sera impresso na tela do usuario.
+    */
    public static void println(boolean x)
    {
       try 
@@ -235,8 +290,12 @@ class MyIO
       {
          e.printStackTrace();
       }
-   }
+   }//fim do metodo println
 
+   /**
+    * Funcao do metodo: Este metodo serve para imprimir um caractere mais uma quebra de linha na tela do usuario.
+    * @param x Caractere que sera impresso na tela do usuario.
+    */
    public static void println(char x)
    {
       try 
@@ -252,8 +311,13 @@ class MyIO
       {
          e.printStackTrace();
       }
-   }
+   }//fim do metodo println
 
+   /**
+    * Funcao do metodo: Este metodo serve para imprimir uma string formatada na tela do usuario
+    * @param formato Formato do que sera impresso.
+    * @param x Numero real de argumento.
+    */
    public static void printf(String formato, double x)
    {
       try 
@@ -269,9 +333,35 @@ class MyIO
       {
          e.printStackTrace();
       }
-   }
+   }//fim printf
 
-   private static double readDouble()
+   /**
+    * Funcao do metodo: Este metodo serve para imprimir uma string formatada na tela do usuario
+    * @param formato Formato do que sera imprsso.
+    * @param args Argumentos da String formatada.
+    */
+   public static void printf(String formato, Object ... args)
+   {
+      try
+      {
+         PrintStream out = new PrintStream(System.out, true, charset);
+         out.printf(formato, args);
+      }
+      catch(UnsupportedEncodingException e)
+      {
+         System.out.println("Erro: charset invalido: "+e);
+      }
+      catch(Exception e)
+      {
+         e.printStackTrace();
+      }
+   }//fim do metodo printf
+
+   /**
+    * Funcao do metodo: Este metodo serve para ler da entrada padrao (teclado) um numero real (do tipo double).
+    * @return Numero real (do tipo double) lido.
+    */
+   public static double readDouble()
    {
       boolean repetir=false;
       double d = -1;
@@ -295,9 +385,14 @@ class MyIO
       while(repetir);
 
       return d;
-   }
+   }//fim do metodo readDouble
 
-   private static double readDouble(String str)
+   /**
+    * Funcao do metodo: Este metodo serve para ler da entrada padrao (teclado) um numero real (do tipo double).
+    * @param str String que sera impressa na tela do usuario antes de efetuar a leitura.
+    * @return Numero real (do tipo double) lido.
+    */
+   public static double readDouble(String str)
    {
       try
       {
@@ -313,18 +408,31 @@ class MyIO
          e.printStackTrace();
       }
       return readDouble();
-   }
+   }//fim do metodo readDouble
 
+   /**
+    * Funcao do metodo: Este metodo serve para ler da entrada padrao (teclado) um numero real (do tipo float).
+    * @return Numero real (do tipo float) lido.
+    */
    public static float readFloat()
    {
       return (float) readDouble();
-   }
+   }//fim do metodo readFloat
 
+   /**
+    * Funcao do metodo: Este metodo serve para ler da entrada padrao (teclado) um numero real (do tipo float).
+    * @param str String que sera impressa na tela do usuario antes de efetuar a leitura.
+    * @return Numero real (do tipo float) que foi lido.
+    */
    public static float readFloat(String str)
    {
       return (float) readDouble(str);
-   }
+   }//fim do metodo readFloat
 
+   /**
+    * Funcao do metodo: Este metodo serve para ler da entrada padrao (teclado) um numero inteiro.
+    * @return Numero inteiro que foi lido.
+    */
    public static int readInt()
    {
       boolean repetir=false;
@@ -348,9 +456,13 @@ class MyIO
       }
       while(repetir);
       return i;
-   }
+   }//fim do metodo readInt
 
-   private static byte readByte()
+   /**
+    * Funcao do metodo: Este metodo serve para ler da entrada padrao (teclado) um numero inteiro (do tipo byte).
+    * @return Numero inteiro (do tipo byte) que foi lido.
+    */
+   public static byte readByte()
    {
       boolean repetir=false;
       byte i=-1;
@@ -374,9 +486,13 @@ class MyIO
       while(repetir);
 
       return i;
-   }
+   }//fim do metodo readByte
 
-   private static short readShort()
+   /**
+    * Funcao do metodo: Este metodo serve para ler da entrada padrao (teclado) um numero inteiro (do tipo short).
+    * @return Numero inteiro (do tipo short) que foi lido.
+    */
+   public static short readShort()
    {
       boolean repetir=false;
       short i=-1;
@@ -400,8 +516,13 @@ class MyIO
       while(repetir);
 
       return i;
-   }
+   }//fim do metodo readShort
 
+   /**
+    * Funcao do metodo: Este metodo serve para ler da entrada padrao (teclado) um numero inteiro. 
+    * @param str String que sera impressa na tela do usuario antes de efetuar a leitura.
+    * @return Numero inteiro que foi lido.
+    */
    public static int readInt(String str)
    {
       try 
@@ -416,6 +537,11 @@ class MyIO
       return readInt();
    }
 
+   /**
+    * Funcao do metodo: Este metodo serve para ler da entrada padrao (teclado) um numero inteiro (do tipo byte).
+    * @param str String que sera impressa na tela do usuario antes de efetuar a leitura.
+    * @return Numero inteiro (do tipo byte) que foi lido.
+    */
    public static byte readByte(String str)
    {
       try 
@@ -430,6 +556,11 @@ class MyIO
       return readByte();
    }
 
+   /**
+    * Funcao do metodo: Este metodo serve para ler da entrada padrao (teclado) um numero inteiro (do tipo short).
+    * @param str String que sera impressa na tela do usuario antes de efetuar a leitura.
+    * @return Numero inteiro (do tipo short) que foi lido.
+    */
    public static short readShort(String str)
    {
       try 
@@ -442,9 +573,13 @@ class MyIO
          System.out.println("Erro: charset invalido: "+e);
       }
       return readShort();
-   }
+   }//fim do metodo readShort
 
-   private static String readString()
+   /**
+    * Funcao do metodo: Este metodo serve para ler da entrada padrao (teclado) uma String.
+    * @return String que foi lida.
+    */
+   public static String readString()
    {
       StringBuilder s = new StringBuilder();
       char tmp;
@@ -465,8 +600,13 @@ class MyIO
          System.out.println("lerPalavra: " + ioe);
       }
       return s.toString();
-   }
+   }//fim do metodo readString
 
+   /**
+    * Funcao do metodo: Este metodo serve para ler da entrada padrao (teclado) uma String.
+    * @param str String que sera impressa na tela do usuario antes de efetuar a leitura.
+    * @return String que foi lida.
+    */
    public static String readString(String str)
    {
       try 
@@ -479,9 +619,13 @@ class MyIO
          System.out.println("Erro: charset invalido: "+e);
       }
       return readString();
-   }
+   }//fim do metodo readString
 
-   private static String readLine()
+   /**
+    * Funcao do metodo: Este metodo serve para ler da entrada padrao (teclado) uma String de uma linha
+    * @return String de uma linha que foi lida.
+    */
+   public static String readLine()
    {
       StringBuilder s = new StringBuilder();
       char tmp;
@@ -502,8 +646,13 @@ class MyIO
          System.out.println("lerPalavra: " + ioe);
       }
       return s.toString();
-   }
+   }//fim do metodo readLine
 
+   /**
+    * Funcao do metodo: Este metodo serve para ler da entrada padrao (teclado) uma String de uma linha.
+    * @param str String que sera impressa na tela do usuario antes de efetuar a leitura.
+    * @return String de uma linha que foi lida.
+    */
    public static String readLine(String str)
    {
       try 
@@ -516,9 +665,13 @@ class MyIO
          System.out.println("Erro: charset invalido: "+e);
       }
       return readLine();
-   }
+   }//fim do metodo readLine
 
-   private static char readChar()
+   /**
+    * Funcao do metodo: Este metodo serve para ler da entrada padrao (teclado) um caractere.
+    * @return Caractere que foi lido.
+    */
+   public static char readChar()
    {
       boolean repetir=false;
       char resp = ' ';
@@ -544,8 +697,13 @@ class MyIO
       while(repetir);
 
       return resp;
-   }
+   }//fim do metodo readChar
 
+   /**
+    * Funcao do metodo: Este metodo serve para ler da entrada padrao (teclado) um caractere.
+    * @param str String que sera impressa na tela do usuario antes de realizar a leitura.
+    * @return Caractere que foi lido.
+    */
    public static char readChar(String str)
    {
       try 
@@ -558,9 +716,13 @@ class MyIO
          System.out.println("Erro: charset invalido: "+e);
       }
       return readChar();
-   }
+   }//fim do metodo readChar
 
-   private static boolean readBoolean()
+   /**
+    * Funcao do metodo: Este metodo serve para ler da entrada padrao (teclado) um valor booleano.
+    * @return Valor booleano lido.
+    */
+   public static boolean readBoolean()
    {
       boolean resp = false;
       boolean repetir=false;
@@ -593,8 +755,13 @@ class MyIO
       }
 
       return resp;
-   }
+   }//fim do metodo readBoolean
 
+   /**
+    * Funcao do metodo: Este metodo serve para ler da entrada padrao (teclado) um valor booleano.
+    * @param str String que sera impressa na tela do usuario antes de realizar a leitura.
+    * @return Valor booleano lido.
+    */
    public static boolean readBoolean(String str)
    {
       try
@@ -607,8 +774,11 @@ class MyIO
          System.out.println("Erro: charset invalido: "+e);
       }
       return readBoolean();
-   }
+   }//fim do metodo readBoolean
 
+   /**
+    * Funcao do metodo: Este metodo serve para pausar a execucao do programa ate que o usuario digite alguma coisa.
+    */
    public static void pause()
    {
       try
@@ -619,8 +789,12 @@ class MyIO
       {
          e.printStackTrace();
       }
-   }
+   }//fim do metodo pause
 
+   /**
+    * Funcao do metodo: Este metodo serve para pausar a execucao do programa ate que o usuario digite alguma coisa.
+    * @param str String que sera impressa na tela do usuario antes de esperar a entrada dele.
+    */
    public static void pause(String str)
    {
       try 
@@ -633,5 +807,5 @@ class MyIO
          System.out.println("Erro: charset invalido: "+e);
       }
       pause();
-   }
+   }//fim do metodo pause
 }
